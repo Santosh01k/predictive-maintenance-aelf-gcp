0b0c93994697e1d87edca818b15d427235a49467
Santosh01k <94747377+Santosh01k@users.noreply.github.com>
2024-07-20 18:26:37 +0545

    deploy_contract.sh

---
+++ aelf/scripts/deploy_contract.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Deploy MaintenanceContract on aelf blockchain
+aelf-cli contract deploy --contract MaintenanceContract.dll --node http://your-aelf-node-api --account your-aelf-account
